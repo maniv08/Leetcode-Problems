aff5b76a88371b5c444c36436d343c0057bd9ece
maniv08 <v.manikandan9841@gmail.com>
2026-08-25 14:38:29 +0530

    Time: 535 ms (81.13%) | Memory: 22.2 MB (81.98%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,21 +1,38 @@
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        target = 0
-        nums.sort()
-        s = set()
-        output = []
-        for i in range(len(nums)):
-            j = i + 1
-            k = len(nums) - 1
-            while j < k:
-                sum = nums[i] + nums[j] + nums[k]
-                if sum == target:
-                    s.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                    k -= 1
-                elif sum < target:
-                    j += 1
+        nums.sort()   # Sort array
+        n = len(nums)
+        res = []      
+
+        for i in range(n - 2):
+            # Skip duplicate
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, n - 1
+            target = -nums[i]
+
+            while left < right:
+                s = nums[left] + nums[right]
+
+                if s == target:
+                    # Found a valid triplet
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicates for left
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    # Skip duplicates for right
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif s < target:
+                    left += 1
                 else:
-                    k -= 1
-        output = list(s)
-        return output
+                    right -= 1
+
+        return res
