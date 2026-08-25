7bf4057ab2463a6ab65e495f7e00b42ef17b2ad3
maniv08 <v.manikandan9841@gmail.com>
2026-08-25 15:04:18 +0530

    Time: 5 ms (37.39%) | Memory: 19.2 MB (60.93%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/remove-linked-list-elements.py
@@ -1,34 +1,20 @@
-# ~ Recursive Approach
-
-class Solution:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
     def removeElements(self, head, val):
-        if head is None:
-            return head
-
-        if head.val == val:
-            return self.removeElements(head.next, val)
-        else:
-            head.next = self.removeElements(head.next, val)
-            return head
-
-
-# _________________________________________________________
-# ~ Iterative Approach
-
-# class Solution:
-#     def removeElements(self, head, val):
-#         dummy = ListNode(0)
-#         dummy.next = head
-
-#         prev = dummy
-#         curr = head
-
-#         while curr is not None:
-#             if curr.val == val:
-#                 prev.next = curr.next
-#             else:
-#                 prev = curr
-
-#             curr = curr.next
-
-#         return dummy.next
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        temp = ListNode(0)
+        temp.next = head
+        prev, curr = temp, head
+        while curr:
+            if curr.val == val:prev.next = curr.next
+            else:prev = curr
+            curr = curr.next
+        return temp.next
